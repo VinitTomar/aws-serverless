01f383b3f90b61cddcfece2e083199dd5c55c888
Vinit Tomar <vinit.tomar68@gmail.com>
2022-08-08 15:39:26 +0000

    form handler added

--- src/app.ts
+++ src/app.ts
@@ -10,24 +10,86 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  *
  */
 
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    let response: APIGatewayProxyResult;
-    try {
-        response = {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello serverless with lambda auth',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        response = {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
-    }
+const formHtml = `
+  <html>
+  <head>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <form method="POST">
+      Please enter your name:
+      <input type="text" name="name"/>
+      <br/>
+      <input type="submit" />
+    </form>
+  </body>
+  </html>
+`;
 
-    return response;
+const thanksHtml = ` <html>
+  <head>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <h1>Thanks</h1>
+    <p>We received your submission</p>
+  </body>
+  </html>
+`;
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const { httpMethod } = event;
+
+  let response: APIGatewayProxyResult;
+  try {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Hello serverless with lambda authorizer',
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'some error happened',
+      }),
+    };
+  }
+
+  return response;
 };
+
+export const formHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const { httpMethod } = event;
+
+  let response: APIGatewayProxyResult;
+  try {
+    if (httpMethod === 'GET') {
+      response = htmlResponse(formHtml);
+    } else {
+      response = htmlResponse(thanksHtml);
+    }
+  } catch (err) {
+    console.log(err);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'some error happened',
+      }),
+    };
+  }
+
+  return response;
+}
+
+function htmlResponse(html: string): APIGatewayProxyResult {
+  return {
+    statusCode: 200,
+    body: html,
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  }
+}
